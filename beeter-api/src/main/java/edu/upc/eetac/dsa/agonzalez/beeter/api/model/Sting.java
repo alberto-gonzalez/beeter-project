e6c0b49a764bafabbb8d562d81bb8d87ac359021
alberto-gonzalez <albertito@hotmail.com>
2015-02-27 16:18:51 +0100

    commit 3!

---
+++ beeter-api/src/main/java/edu/upc/eetac/dsa/agonzalez/beeter/api/model/Sting.java
@@ -0,0 +1,53 @@
+package edu.upc.eetac.dsa.agonzalez.beeter.api.model;
+
+public class Sting {
+public int getSttingid() {
+		return sttingid;
+	}
+	public void setSttingid(int sttingid) {
+		this.sttingid = sttingid;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public long getLasModified() {
+		return lasModified;
+	}
+	public void setLasModified(long lasModified) {
+		this.lasModified = lasModified;
+	}
+	public long getCreationTimestamp() {
+		return creationTimestamp;
+	}
+	public void setCreationTimestamp(long creationTimestamp) {
+		this.creationTimestamp = creationTimestamp;
+	}
+private int sttingid;
+private String username;
+private String author;
+private String subject;
+private String content;
+private long lasModified;
+private long creationTimestamp;
+}
